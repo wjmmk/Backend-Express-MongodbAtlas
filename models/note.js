9d96141465fc1cc855eddd58d8a7af05903cf754
William Mosquera <wjmmk@gmail.com>
2021-06-07 08:36:57 -0500

    Initial Charge

--- models/note.js
+++ models/note.js
@@ -1,27 +1,13 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
-    content: {
-        type: String,
-        minlength: 5,
-        required: true
-      },
-      date: { 
-        type: Date,
-        required: true
-      },
-     important: Boolean
+  content: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  date: Date,
+  important: Boolean,
 })
 
 noteSchema.set('toJSON', {
