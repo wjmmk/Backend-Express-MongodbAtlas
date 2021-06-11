f7bf457f676bf574e2bce624cc102073afb5eaf5
William Mosquera <wjmmk@gmail.com>
2021-06-11 17:06:49 -0500

    Refactoring App

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,6 +1,7 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
+
 notesRouter.get('/', (request, response) => {
   Note.find({}).then(notes => {
     response.json(notes.map(note => note.toJSON()))
--- controllers/users.js
+++ controllers/users.js
@@ -11,7 +11,7 @@ usersRouter.post('/', async (request, response) => {
   const user = new User({
     username: body.username,
     name: body.name,
-    passwordHash,
+    passwordHash,  
   })
 
   const savedUser = await user.save()
@@ -19,7 +19,7 @@ usersRouter.post('/', async (request, response) => {
   response.json(savedUser)
 })
 
-usersRouter.get('/', async (request, response) => {
+usersRouter.get('/', async (response) => {
   const users = await User.find({})
   response.json(users.map(u => u.toJSON()))
 })
